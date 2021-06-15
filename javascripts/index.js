d4ed3cc53e47bf03fcbfb3b1b0566fb2754e6215
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-06-15 23:50:08 +0100

    work on submit event fot the form to submit the data

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,11 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     createPostForm().addEventListener("submit", (e) => createFormHandler(e))
 })
 const handleClick = () => {
+    if (ul().children.length < 1){
     fetch(endPoint)
     .then(resp => resp.json())
     .then(json => renderPosts(json))
     .catch(err => console.log(err))
+} else {
+ul().innerHTML = ""
+ }
 }
+
 const renderPosts = (posts) => {
  posts.forEach(element => {
  const li = document.createElement("li")
@@ -54,6 +59,19 @@ function postFetch(brand, image, size, price, website, description, category_id)
  .then(post => {
      debugger
  console.log(post);
+ const renderPosts = posts
+    const li = document.createElement("li")
+    li.innerHTML = `
+       <h2 class="post-category">${element.category.name}</h2>
+       <h2 class="post-brand">${element.brand}</h2>
+       <h2 class="post-image_url">
+           <img src=${element.image_url} height="200" width="250"></h2>
+       <h2 class="post-size">${element.size}</h2>
+       <p class="post-description">${element.description}</p>
+       <h4 class="post-price">${element.price}</h4>
+       <h2 class="post-website">${element.website}</h2>
+       <button data-id=${posts.id}>edit</button>
+    `
+    ul().appendChild(li)   
  })
- .catch(err => console.log(err))
 }
