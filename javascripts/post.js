77a46d5738633c8ca24f3d30045dcbdf37050900
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-06-18 15:38:53 +0100

    working on frontend styling using bootstrap

--- javascripts/post.js
+++ javascripts/post.js
@@ -1,6 +1,6 @@
 class Post {
     static all = []
-    constructor({id, brand, image, size, description, price, website, category_id}) {
+    constructor({id, brand, image, size, description, price, website, category_id, category}) {
         this.id = id
         this.brand = brand
         this.image = image
@@ -9,6 +9,7 @@ class Post {
         this.price = price
         this.website = website
         this.category_id = category_id
+        this.category = category
         Post.all.push(this)
     }
 
@@ -18,17 +19,40 @@ class Post {
     renderPost() {
         const li = document.createElement("li")
         li.innerHTML = `
-           <h2 class="post-category">${this.findCategoryById().name}</h2>
-           <h2 class="post-brand">${this.brand}</h2>
-           <h2 class="post-image">
-               <img src=${this.image} height="200" width="250"></h2>
-           <h2 class="post-size">${this.size}</h2>
-           <p class="post-description">${this.description}</p>
-           <h4 class="post-price">${this.price}</h4>
-           <h2 class="post-website">${this.website}</h2>
-           <button class="edit-post" data-id="${this.id}">edit</button> 
-           <button class="delete-post" data-id="${this.id}">delete</button> 
-        `
+
+        <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+        <img src=${this.image} class="post-img-top" alt="...">
+          <div class="post-body">
+          <h2 class="post-category">${this.findCategoryById().name}</h2>
+          <h5 class="post-brand">${this.brand}</h5>
+          <h5 class="post-size">size-${this.size}</h5>
+          <h5 class="post-price">£${this.price}</h5>
+          <p class="post-description">${this.description}</p>
+          <h5 class="post-website">${this.website}</h5>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+              </div>
+              <small class="text-muted">Category: ${this.category.name}</small>
+            </div>
+          </div>
+        </div>
+      </div>`
+      
+
+           //<h2 class="post-category">${this.findCategoryById().name}</h2>
+           //<h2 class="post-brand">${this.brand}</h2>
+           //<h2 class="post-image">
+              // <img src=${this.image} height="200" width="250"></h2>
+           //<h2 class="post-size">${this.size}</h2>
+          // <p class="post-description">${this.description}</p>
+           //<h4 class="post-price">${this.price}</h4>
+           //<h2 class="post-website">${this.website}</h2>
+          // <button class="edit-post" data-id="${this.id}">edit</button> 
+          // <button class="delete-post" data-id="${this.id}">delete</button> 
+       // `
         ul().appendChild(li)
        }
        
